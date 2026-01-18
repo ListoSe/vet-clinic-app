e9f43331436a7214c6991f9bb0e6587f07a5b53c
ListoSe <122672762+ListoSe@users.noreply.github.com>
2026-01-18 01:31:30 +0100

    Api_fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'http://localhost:3000/api',
   timeout: 1000,
-  headers: {'Content-Type': 'application/json'}
+  headers: { 'Content-Type': 'application/json' },
 });
 
 export default instance;
